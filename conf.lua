2a7b0a2871de75ecf15ca4a5a4e647b21b9de875
SOLN4R <soln4r@gmail.com>
2025-01-03 21:38:52 +0300

    Initial configuration setup

--- conf.lua
+++ conf.lua
@@ -1,2 +1,14 @@
 function love.conf(t)
+
+    t.version = "11.5"
+    t.console = true
+
+    t.window.title = "Top-Down Shooter"
+    t.window.width = 1920
+    t.window.height = 1080
+    t.window.resizable = true
+    t.window.fullscreen = false
+    t.window.fullscreentype = "desktop"
+    t.window.vsync = 1
+
 end
